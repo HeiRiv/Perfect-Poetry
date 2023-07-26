96c6c99494bce6ea1f9c48f8c233f4dcc02102cb
Heidy Beltran <HeidyBeltran@Alains-MacBook-Air.local>
2023-07-26 10:04:35 -0400

    Organized files

--- models/index.js
+++ models/index.js
@@ -1,5 +1,10 @@
 const User = require('./User');
 const Poetry = require('./Poetry');
+let savePoemBtn = undefined;
+
+if (window.location.pathname === '/profile') {
+  savePoemBtn = document.querySelector('.save-poem');
+}
 
 User.hasMany(Poetry, {
   foreignKey: 'user_id',
@@ -9,5 +14,29 @@ User.hasMany(Poetry, {
 Poetry.belongsTo(User, {
   foreignKey: 'user_id',
 });
+
+const savePoem = (poem) =>
+  fetch('/api/poems', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(poem),
+  });
+
+const handlePoemSave = () => {
+  const newPoem = {
+    title: PoemTitle.value,
+    text: PoemText.value,
+  };
+  savePoem(newPoem).then(() => {
+    getAndRenderPoems();
+    renderActivePoem();
+  });
+};
+
+if (window.location.pathname === '/profile') {
+  savePoemBtn.addEventListener('click', handlePoemSave);
+}
 
 module.exports = { User, Poetry };
